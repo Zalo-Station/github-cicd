903786628cc679fa5d9a71c8931bfea808ca1c3a
Rabin Rai <rabinrai44@gmail.com>
2021-12-19 12:20:52 -0500

    Added example spring boot app

---
+++ src/main/java/com/zalostation/githubcicd/GithubCicdActionApplication.java
@@ -0,0 +1,21 @@
+package com.zalostation.githubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to zalostation.com";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zalostation/githubcicd/GithubCicdActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalostation.githubcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
